e09ee29d683350df175177d6504df80c9e77e060
Shantanu Rastogi <shanrastogi2573@gmail.com>
2026-08-13 13:29:13 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> mp;
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]] = i;
+        }
+
+        vector<int> ans;
+        int size = 0;
+        int end = 0;
+        for (int i = 0; i < s.size(); i++) {
+            size++;
+            end = max(end, mp[s[i]]);
+            if (i == end) {
+                ans.push_back(size);
+                size = 0;
+            }
+        }
+        return ans;
+    }
+};
